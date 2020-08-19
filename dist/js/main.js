bd08228c5d55b45b077163af4bed934d1e771ccb
Rafael <rafaelarturo16@gmail.com>
2020-08-18 20:32:30 -0400

    calculadora tres

--- dist/js/main.js
+++ dist/js/main.js
@@ -251,7 +251,7 @@ function appendToTable(input, tipo) {
                         }else{
                             cambiarTotal(-parseFloat(dif));
                         }
-            
+
                         return 0;
                     });
 
@@ -411,3 +411,58 @@ $("#guardar").on("click", function() {
 
 
 } );
+
+
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+const display = document.querySelector('.calculator__display')
+
+
+keys.addEventListener("click", e => {
+ if (e.target.matches("button")) {
+     const key = e.target
+     const action = key.dataset.action
+     const keyContent = key.textContent
+     const displayedNum = display.textContent
+
+
+        if (!action) {
+        console.log('number key!')
+
+        if (displayedNum === '0') {
+          display.textContent = keyContent
+        } else {
+            display.textContent = displayedNum + keyContent
+        }
+
+        }
+
+        if(action === 'add'){
+            appendToTable(display.textContent,1);
+            display.textContent = '0'
+        }
+
+        if(action === 'subtract'){
+            appendToTable(display.textContent,0);
+            display.textContent = '0'
+        }
+
+        if ( action === 'multiply' ) {
+          display.textContent = displayedNum + '*';
+        }
+
+        if (action === 'decimal') {
+         console.log('decimal key!')
+          display.textContent = displayedNum + '.'
+        }
+
+        if (action === 'clear') {
+          display.textContent = '0'
+        }
+
+        if (action === 'calculate') {
+          appendToTable(display.textContent,1);
+          display.textContent = '0'
+        }
+ }
+});
