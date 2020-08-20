a38da6a0de03d68da0b1f7bf4f310e476e551057
Rafael <rafaelarturo16@gmail.com>
2020-08-20 18:10:16 -0400

    button

--- bower_components/bootstrap/js/button.js
+++ bower_components/bootstrap/js/button.js
@@ -123,23 +123,3 @@
     })
 
 }(jQuery);
-
-
-
-$(document)
-  .on('click.bs.button.data-api', '[data-toggle^="button"]', function (e) {
-    var $btn = $(e.target).closest('.btnmio')
-    Plugin.call($btn, 'toggle')
-    if (!($(e.target).is('input[type="radio"], input[type="checkbox"]'))) {
-      // Prevent double click on radios, and the double selections (so cancellation) on checkboxes
-      e.preventDefault()
-      // The target component still receive the focus
-      if ($btn.is('input,button')) $btn.trigger('focus')
-      else $btn.find('input:visible,button:visible').first().trigger('focus')
-    }
-  })
-  .on('focus.bs.button.data-api blur.bs.button.data-api', '[data-toggle^="button"]', function (e) {
-    $(e.target).closest('.btnmio').toggleClass('focus', /^focus(in)?$/.test(e.type))
-  })
-
-}(jQuery);
